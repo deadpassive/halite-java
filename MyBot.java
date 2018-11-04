9a648a415d4c414fda8affb96b87b035c50ddfec
JakeForsey <jakeforsey@googlemail.com>
2018-11-04 20:32:35 +0000

    Automatically create replay folder if it doesnt already exist

--- MyBot.java
+++ MyBot.java
@@ -1,10 +1,23 @@
 import models.bots.SimpleBot;
 
+import java.io.File;
+import java.util.logging.Logger;
+
 /**
  * This is just the class we use to run and make halite happy.
  */
 public class MyBot {
+    private static final Logger LOGGER = Logger.getLogger(MyBot.class.getName());
+
     public static void main(final String[] args) {
+        File replayFolder = new File("replays");
+
+        if (!replayFolder.exists()) {
+            LOGGER.info("Replay folder doesn't exist");
+            replayFolder.mkdir();
+            LOGGER.info("Replay folder created");
+        }
+
         SimpleBot.getInstance().run(args);
     }
 }

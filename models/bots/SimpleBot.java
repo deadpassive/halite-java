10bb99842683862ff4df19a20ec2318c37ac72f2
Jon Britton <jon.britton@field-dynamics.co.uk>
2018-11-04 17:57:45 +0000

    added abstract ship and bot

---
+++ models/bots/SimpleBot.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Dotted Eyes Ltd 2018.
+ * All Rights Reserved.
+ *
+ * This Software is the confidential information of
+ * Dotted Eyes Ltd. 67-71 Northwood St,
+ * Birmingham, B3 1TX, United Kingdom.
+ *
+ * The software may be used only in accordance with the terms
+ * of the licence agreement made with Dotted Eyes Ltd.
+ *
+ */
+package models.bots;
+
+public class SimpleBot {
+}

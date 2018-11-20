440ca03bc1ec3a33cfc00b0bc4b68c095ef27e39
JakeForsey <jakeforsey@googlemail.com>
2018-11-20 08:25:31 +0000

    Fixed omissions

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 set -e
-./halite --replay-directory replays/ -vvv "java -jar target/MyBot.jar hand-tuned" "java -jar target/MyBot.jar optimized"
+./halite --replay-directory replays/ --height 64 --width 64 -vvv "java -jar target/MyBot.jar hand-tuned" "java -jar target/MyBot.jar optimized"
+sleep 100
 ./halite --replay-directory replays/ -vvv "java -jar target/MyBot.jar hand-tuned" "java -jar target/MyBot.jar optimized" "java -jar target/MyBot.jar random" "java -jar target/MyBot.jar optimized"
